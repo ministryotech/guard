960e7b01dded4d4d9160de503a9ea6c0d381a617
Keith Jackson <keith@ministryotech.co.uk>
2018-03-22 18:13:38 +0000

    Initial commit

---
+++ arg-guard.js
@@ -0,0 +1,86 @@
+/*
+    Argument Guard
+    --------------
+    Throws well formatted errors for guarding methods.
+
+    https://github.com/ministryotech/guard
+*/
+(function() {
+
+    const root = window
+
+    const argumentGuard = {
+
+        /**
+         * Throws a {TypeError} if the required parameter is null or undefined.
+         * 
+         * @param {any} param The parameter to guard.
+         * @param {string} paramName The optional name of the parameter.
+         */
+        throwIf: function(param, paramName) {
+            if (param === undefined || param === null) throw TypeError(paramName 
+                ? `The '${paramName}' parameter is required.`
+                : `The parameter is required.`)
+        },
+    
+        /**
+         * Throws a {TypeError} if the required parameter is not in the range of expected values.
+         * 
+         * @param {any} param The parameter to guard.
+         * @param {string} paramName The optional name of the parameter.
+         */
+        throwIfNotIn: function(param, validValues, paramName) {
+            argumentGuard.throwIf(validValues, "validValues")      
+            argumentGuard.throwIf(param, paramName) 
+            if (!validValues.includes(param)) throw TypeError(paramName 
+                ? `The '${paramName}' parameter is out of range.`
+                : `The parameter is out of range.`)
+        },
+        
+        /**
+         * Throws a {TypeError} if the required parameter is null.
+         * 
+         * @param {any} param The parameter to guard.
+         * @param {string} paramName The optional name of the parameter.
+         */
+        throwIfNull: function(param, paramName) {
+            if (param === null) throw TypeError(paramName 
+                ? `The '${paramName}' parameter cannot be null.`
+                : `The parameter cannot be null.`)
+        },
+        
+        /**
+         * Throws a {TypeError} if the required parameter is undefined.
+         * 
+         * @param {any} param The parameter to guard.
+         * @param {string} paramName The optional name of the parameter.
+         */
+        throwIfUndefined: function(param, paramName) {
+            if (param === undefined) throw TypeError(paramName 
+                ? `The '${paramName}' parameter must be defined.`
+                : `The parameter must be defined.`)
+        },
+        
+        /**
+         * Throws a {TypeError} if the specified parameter is not a number.
+         * 
+         * @param {any} param The parameter to guard.
+         * @param {string} paramName The optional name of the parameter.
+         */
+        throwIfNaN: function(param, paramName) {
+            if (isNaN(Number(param))) throw TypeError(paramName 
+                ? `The '${paramName}' parameter is not a number.`
+                : `The parameter is not a number.`)
+        }
+    }
+
+    // Exports to the global scope
+    if (typeof define === "function" && define.amd) {
+        define("arg-guard", [], function() { return argumentGuard })
+    } else if (typeof exports === "object") {
+        module.exports = argumentGuard
+    } else {
+        root.ArgumentGuard = argumentGuard
+    }
+    
+})()
---
+++ readme.md
@@ -0,0 +1,41 @@
+# Introduction
+This project provides simple guard methods for throwing appropriate type errors when arguments are incorrect.
+
+Variants supported...
+
+- throwIf() - Throws if the parameter is null or undefined.
+- throwIfNotIn() - Throws if the required parameter is not in the range of expected values.
+- throwIfNull() - Throws if the required parameter is null.
+- throwIfUndefined() - Throws if the parameter is undefined.
+- throwIfNaN() - Throws if the specified parameter is not a number.
+
+Sample usage
+```
+const guard = require("arg-guard");
+
+const myFunc = function(requiredArg, otherArg) {
+    guard.throwIf(requiredArg, "requiredArg");
+}
+```
+
+# The Ministry of Technology Open Source Products
+Welcome to The Ministry of Technology open source products. All open source Ministry of Technology products are distributed under the MIT License for maximum re-usability. Details on more of our products and services can be found on our website at http://www.ministryotech.co.uk
+
+Our other open source repositories can be found here...
+
+* [https://bitbucket.org/ministryotech](https://bitbucket.org/ministryotech)
+* [https://github.com/ministryotech](https://github.com/ministryotech)
+* [https://github.com/tiefling](https://github.com/tiefling)
+
+Newer content prefers Github. Bitbucket is no longer actively used.
+
+### Where can I get it?
+You can download the package for this project from any of the following package managers...
+
+- **NPM** - [https://www.mpmjs.com/uri-path-manager](https://www.mpmjs.com/uri-path-manager)
+
+### Contribution guidelines
+If you would like to contribute to the project, please contact me.
+
+### Who do I talk to?
+* Keith Jackson - keith@ministryotech.co.uk
