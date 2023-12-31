e29943a89f9ef82eaba9252eeaae5635e12862d8
Keith Jackson <temporal-net@live.co.uk>
2023-12-31 21:54:40 +0000

    New version

--- src/arg-guard.js
+++ src/arg-guard.js
@@ -8,12 +8,12 @@
 */
 (function() {
 
-    const root = window
+    var root = window
 
     /**
      * A helper for guarding against null / undefined arguments.
      */
-    const ArgumentGuard = {
+    var argumentGuard = {
 
         /**
          * Throws if the required parameter is null or undefined.
@@ -22,11 +22,13 @@
          * @throws {TypeError}
          */
         throwIf: function(param, paramName) {
-            if (param === undefined || param === null) throw TypeError(paramName 
-                ? 'The ' + paramName + ' parameter is required.'
-                : 'The parameter is required.')
+            if (param === undefined || param === null) {
+                throw new TypeError(paramName
+                    ? 'The ' + paramName + ' parameter is required.'
+                    : 'The parameter is required.')
+            }
         },
-    
+
         /**
          * Throws if the required parameter is not in the range of expected values.
          * @param {any} param The parameter to guard.
@@ -35,13 +37,15 @@
          * @throws {TypeError}
          */
         throwIfNotIn: function(param, validValues, paramName) {
-            ArgumentGuard.throwIf(validValues, 'validValues')
-            ArgumentGuard.throwIf(param, paramName)
-            if (!validValues.includes(param)) throw TypeError(paramName 
-                ? 'The ' + paramName + ' parameter is out of range.'
-                : 'The parameter is out of range.')
+            argumentGuard.throwIf(validValues, 'validValues')
+            argumentGuard.throwIf(param, paramName)
+            if (!validValues.includes(param)) {
+                throw new TypeError(paramName
+                    ? 'The ' + paramName + ' parameter is out of range.'
+                    : 'The parameter is out of range.')
+            }
         },
-        
+
         /**
          * Throws if the required parameter is null.
          * @param {any} param The parameter to guard.
@@ -49,11 +53,13 @@
          * @throws {TypeError}
          */
         throwIfNull: function(param, paramName) {
-            if (param === null) throw TypeError(paramName 
-                ? 'The ' + paramName + ' parameter cannot be null.'
-                : 'The parameter cannot be null.')
+            if (param === null) {
+                throw new TypeError(paramName
+                    ? 'The ' + paramName + ' parameter cannot be null.'
+                    : 'The parameter cannot be null.')
+            }
         },
-        
+
         /**
          * Throws if the required parameter is undefined.
          * @param {any} param The parameter to guard.
@@ -61,11 +67,13 @@
          * @throws {TypeError}
          */
         throwIfUndefined: function(param, paramName) {
-            if (param === undefined) throw TypeError(paramName 
-                ? 'The ' + paramName + ' parameter must be defined.'
-                : 'The parameter must be defined.')
+            if (param === undefined) {
+                throw new TypeError(paramName
+                    ? 'The ' + paramName + ' parameter must be defined.'
+                    : 'The parameter must be defined.')
+            }
         },
-        
+
         /**
          * Throws if the specified parameter is not a number.
          * @param {any} param The parameter to guard.
@@ -73,10 +81,12 @@
          * @throws {TypeError}
          */
         throwIfNaN: function(param, paramName) {
-            if (isNaN(Number(param))) throw TypeError(paramName 
-                ? 'The ' + paramName + ' parameter is not a number.'
-                : 'The parameter is not a number.')
-        }
+            if (isNaN(Number(param))) {
+                throw new TypeError(paramName
+                    ? 'The ' + paramName + ' parameter is not a number.'
+                    : 'The parameter is not a number.')
+            }
+        },
     }
 
     /*--------------------------------------------------------------------------*/
@@ -85,11 +95,16 @@
     // noinspection JSUnresolvedReference - define check for require.js module support.
     if (typeof define === 'function' && define.amd) {
         // noinspection JSUnresolvedReference - define check for require.js module support.
-        define('arg-guard', [], function() { return ArgumentGuard })
-    } else if (typeof exports === 'object') {
-        module.exports = ArgumentGuard
-    } else {
-        root.ArgumentGuard = ArgumentGuard
+        define('arg-guard', [], function() {
+            return argumentGuard
+        })
+    } else { // noinspection JSUnresolvedReference
+        if (typeof exports === 'object') {
+            // noinspection JSUnresolvedReference
+            module.exports = argumentGuard
+        } else {
+            root.ArgumentGuard = argumentGuard
+        }
     }
-    
+
 })()
